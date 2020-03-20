f9f95a9b9e5318130f2368bbe71e34ef4f1b0eb6
Forrest Vaughan <forrest@v6carolina.com>
2020-03-19 22:33:28 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace AccessDBtest
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ForrestV\Documents\Database1.accdb;Persist Security Info=False;";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                
+                connection.Open();
+                checkConnection.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error " + ex);
+            }
+        }
+    }
+}
